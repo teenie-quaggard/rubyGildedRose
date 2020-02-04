15dd68578e75729bad70707672b81f43c01d940c
Kristina Jaggard <kjaggard@8thlight.com>
2020-02-04 10:39:12 +0000

    initial project set up

---
+++ gilded_rose_tests.rb
@@ -0,0 +1,14 @@
+# require File.join(File.dirname(__FILE__), '../gilded_rose')
+require 'test/unit'
+# require 'gilded_rose'
+
+class TestUntitled < Test::Unit::TestCase
+
+  def test_foo
+    items = [Item.new("foo", 0, 0)]
+    GildedRose.new(items).update_quality()
+    assert_equal items[0].name, "foo"
+  end
+
+end
+
